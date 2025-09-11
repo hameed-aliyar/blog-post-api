42fb83e8d61468faafa4672e3cfddfbda1ee2a87
Abdul Hameed T A <hameedamithattuparambil@gmail.com>
2025-09-12 00:11:45 +0530

    refactored getPostById.

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -14,15 +14,16 @@ const getPosts = async (req, res, next) => {
 };
 
 const getPostById  = async (req, res, next) => {
-    const { id } = req.params;
     try {
-        const sqlQuery = 'SELECT * FROM posts WHERE id = $1';
-        const values = [id];
-        //const result = await pool.query(sqlQuery, values);
-        if (result.rows.length === 0) {
-            return res.status(404).json({ message: `Post with ID ${id} not found` });
+        const idToFind = parseInt(req.params.id);
+        const post = await prisma.post.findUnique({
+            where: {
+                id: idToFind,
+            },
+        });
+        if (!post) {
+            return res.status(404).json({ message: `Post with ID ${idToFind} not found` });
         }
-        const post = result.rows[0];
         res.json(post);
     } catch (error) {
         next(error);
